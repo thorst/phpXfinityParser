8fde8ccba3efbab9c695426d3cdc284ed11d9e2c
Todd Horst <toddmhorst@yahoo.com>
2014-01-06 21:59:14 -0500

    started index and readme

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ phpXfinityParser
 ================
 
 Parse movies list, provider better sorting
+
+
+Hosting:
+	http://cpanel.1freehosting.com/
+URL:	http://xfinity.pixub.com/
+Client:
+	Bootstrap:http://getbootstrap.com/
+	jQuery
+	jsRender
+Service:
+	Php:
+	SimplePhpWebparser
---
+++ index.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Navbar Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+	
+	<style>
+	body {
+  padding-top: 50px;
+}
+</style>
+</head>
+<body>
+<div class="container">
+
+     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+<table class="table table-hover table-stripped">
+<thead>
+<tr>
+<th>Title</th>
+<th>Added</th>
+</tr>
+</thead>
+<tbody>
+<?php
+include('config.php');
+$mysqli = new mysqli("localhost", DB_USER,DB_PASSWORD,DB_NAME);
+if ($result = $mysqli->query("SELECT * FROM movies Where removed is null")) {
+	while($obj = $result->fetch_object()){
+		echo "
+		<tr>
+			<td>".$obj->title."</td>
+			<td>".$obj->inserted."</td>
+			
+		</tr>
+		";
+    } 
+	$result->close();
+}
+?>
+</tbody>
+</table>
+</div>
+</body>
+</html>
